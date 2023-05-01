6f548bdf4bf0ba6406a8651665bccacbafc619bc
Joshim Uddin <mju881@gmail.com>
2023-05-01 21:48:50 +0600

    index.js file setup done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-node_modules
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
