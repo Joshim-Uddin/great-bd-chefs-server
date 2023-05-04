745fb2ec1d91fdd8c54c6659b5151152d5c179f0
Joshim Uddin <mju881@gmail.com>
2023-05-04 19:34:41 +0600

    done

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json')
 const popular = require('./data/popularRecipes.json')
+const featured = require('./data/featured.json')
+const blog = require('./data/blog.json')
 
 app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.send([chefs, popular])
+    res.send(chefs)
+})
+app.get('/popular', (req, res) => {
+    res.send(popular)
+})
+app.get('/featured', (req, res) => {
+    res.send(featured)
+})
+app.get('/blog', (req, res) => {
+    res.send(blog)
 })
 
 app.get('/chefs/:id', (req, res) => {
