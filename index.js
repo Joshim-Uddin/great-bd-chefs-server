d62a561bef520d000f8ef5f8e6bd59a527f3e314
Joshim Uddin <mju881@gmail.com>
2023-05-04 08:54:18 +0600

    popular recipes added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json')
+const popular = require('./data/popularRecipes.json')
+
+app.use(cors())
+
+
 app.get('/', (req, res) => {
-    res.send('Server is running')
+    res.send([chefs, popular])
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const anotherChefs = chefs.chefs;
+    const id = req.params.id;
+    const recipes = anotherChefs.find(chef => chef.id == id)
+
+    res.send(recipes.recipes)
 })
 
 app.listen(port, () => {
